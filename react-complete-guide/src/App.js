872017c993f235543017372992bb4c01099e78c1
Dan McKinney <3dhdaniel@gmail.com>
2019-03-27 10:52:18 -0400

    3rd

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
+
+
 
 class App extends Component {
   render() {
@@ -7,6 +10,9 @@ class App extends Component {
       <div className='App'>
         <h1>Hi, I'm a react app</h1>
         <p>This is really working</p>
+        <Person name='max'age='28'/>
+        <Person name='manu'age='29'>My Hobbies</Person>
+        <Person name='stephanie'age='30'/>
         </div>
     );
   // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now'))
